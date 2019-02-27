17c0257f8834ecd82301f72c2c86ab8dc8f8dfd7
wzj <wzj@wzj>
2019-02-27 22:15:00 +0800

    x = '3'应该是错的

--- Main.py
+++ Main.py
@@ -1,10 +1,3 @@
-str_split = input().split()
-a = float(str_split[0])
-b = float(str_split[1])
-c = float(str_split[2])
-ans = b * b - 4 * a * c
-if float(round(ans)) == ans:
-    ans = round(ans)
-print(ans)
-
+x = 3
+print(x == '3')
 
